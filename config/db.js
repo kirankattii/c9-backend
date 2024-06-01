8fa282a02c605bb4793d35e027ebdfaace08ba43
Kiran Katti <139916130+kirankattii@users.noreply.github.com>
2024-06-01 14:09:05 +0530

    update db

--- config/db.js
+++ config/db.js
@@ -5,10 +5,7 @@ dotenv.config()
 export const connectDB = async () => {
 	console.log("DB_URI:", process.env.DB_URI) // Debugging line
 	try {
-		await mongoose.connect(process.env.DB_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		})
+		await mongoose.connect(process.env.DB_URI)
 		console.log("DB connected")
 	} catch (error) {
 		console.error("DB connection error:", error.message)
